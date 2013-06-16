2fc7ffc6611ea55d851666117ca164bd24893336
jmone <jmone@localhost.localdomain>
2013-06-17 01:03:37 +0800

    new sql

--- docs/swordfish.sql
+++ docs/swordfish.sql
@@ -1,119 +1,152 @@
--- phpMyAdmin SQL Dump
--- version 3.4.8
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.1.60, for unknown-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Jun 13, 2013 at 01:18 AM
--- Server version: 5.1.60
--- PHP Version: 5.3.25
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: swordfish
+-- ------------------------------------------------------
+-- Server version	5.1.60-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `swordfish`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `default_dict`
 --
 
-CREATE TABLE IF NOT EXISTS `default_dict` (
+DROP TABLE IF EXISTS `default_dict`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `default_dict` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `word` char(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `word` (`word`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `default_dict`
+--
+
+LOCK TABLES `default_dict` WRITE;
+/*!40000 ALTER TABLE `default_dict` DISABLE KEYS */;
+/*!40000 ALTER TABLE `default_dict` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `ext_dict`
 --
 
-CREATE TABLE IF NOT EXISTS `ext_dict` (
+DROP TABLE IF EXISTS `ext_dict`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ext_dict` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `word` char(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `word` (`word`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `ext_dict`
+--
+
+LOCK TABLES `ext_dict` WRITE;
+/*!40000 ALTER TABLE `ext_dict` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ext_dict` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `search_favorite`
 --
 
-CREATE TABLE IF NOT EXISTS `search_favorite` (
+DROP TABLE IF EXISTS `search_favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `search_favorite` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `text` varchar(200) NOT NULL,
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `search_favorite`
+--
+
+LOCK TABLES `search_favorite` WRITE;
+/*!40000 ALTER TABLE `search_favorite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `search_favorite` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `stop_word`
 --
 
-CREATE TABLE IF NOT EXISTS `stop_word` (
+DROP TABLE IF EXISTS `stop_word`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stop_word` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `word` char(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `word` (`word`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `stop_word`
 --
 
-INSERT INTO `stop_word` (`id`, `word`) VALUES
-(1, '的'),
-(2, '一个'),
-(3, '了'),
-(4, '在'),
-(5, '自己'),
-(6, '是'),
-(7, '和'),
-(8, '会'),
-(9, '都'),
-(10, '没有'),
-(11, '着');
-
--- --------------------------------------------------------
+LOCK TABLES `stop_word` WRITE;
+/*!40000 ALTER TABLE `stop_word` DISABLE KEYS */;
+INSERT INTO `stop_word` VALUES (1,'的'),(2,'一个'),(3,'了'),(4,'在'),(5,'自己'),(6,'是'),(7,'和'),(8,'会'),(9,'都'),(10,'没有'),(11,'着');
+/*!40000 ALTER TABLE `stop_word` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   `email` varchar(200) NOT NULL,
   `password` varchar(100) NOT NULL,
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `dateline`) VALUES
-(1, 'jmone2006@126.com', '95d297d18efa0670acd32bc6a1544ff7', 1371051063),
-(4, 'jim@exephp.com', 'ee6af8dbc8b2af80581246966eaeea44', 1371057338),
-(3, 'admin@exephp.com', '41759c4b6e9e56a8ab18191b98d03030', 1371051181),
-(5, 'jiangming@xiaomi.com', '3855771f0912e76294305e43080d93ef', 1371057409);
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,1,'jmone2006@126.com','95d297d18efa0670acd32bc6a1544ff7',1371051063),(4,0,'jim@exephp.com','ee6af8dbc8b2af80581246966eaeea44',1371057338),(3,0,'admin@exephp.com','41759c4b6e9e56a8ab18191b98d03030',1371051181);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-06-17  1:02:51
