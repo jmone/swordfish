4a19f6433ca5e13401bf6c8e68d2ded7d6c23b5d
jmone <jmone@jmone-pc.(none)>
2013-08-10 08:50:38 +0800

    sdsds

--- crawl/amazon.php
+++ crawl/amazon.php
@@ -39,7 +39,7 @@ while ($url = array_pop($uncrawled_urls)){
 	$links = parse_links($content);
 	foreach ($links as $link){
 		if(!in_array($link, $crawled_urls) && !in_array($link, $uncrawled_urls)){
-			if(stripos($link, 'amazon.cn') === false || stripos($link, 'signin') !== false){
+			if(stripos($link, 'amazon.cn') === false || stripos($link, 'signin') !== false || stripos($link, 'forum') !== false){
 				continue;
 			}
 			array_push($uncrawled_urls, $link);
